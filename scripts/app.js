9cd8f95655fd08a34d0ec15a48d0a5845626e209
amir200h <setting200h@gmail.com>
2024-07-23 03:25:34 +0330

    add humberger menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 const navBtn = document.querySelector(".nav__btn");
-
+const mobileMenu = document.querySelector(".menu-mobile");
 navBtn.addEventListener("click", () => {
   navBtn.classList.toggle("nav__btn--open");
+  mobileMenu.classList.toggle("menu-mobile--open");
 });
